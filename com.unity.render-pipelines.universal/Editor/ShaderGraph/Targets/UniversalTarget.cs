8838d5061d76dff6ec7d7171601e863c43d9a14c
Joshua Davis <joshua.davis@unity3d.com>
2021-05-06 14:00:44 -0700

    Formatting fix

--- com.unity.render-pipelines.universal/Editor/ShaderGraph/Targets/UniversalTarget.cs
+++ com.unity.render-pipelines.universal/Editor/ShaderGraph/Targets/UniversalTarget.cs
@@ -829,7 +829,6 @@ namespace UnityEditor.Rendering.Universal.ShaderGraph
             AddAlphaClipControlToPass(ref result, target);
 
             return result;
-            
         }
 
         public static PassDescriptor _2DScenePicking(UniversalTarget target)
